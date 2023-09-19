53ea4255ecd866fc93dd5ee133d80a33e208ce75
Sophia <chikakusophia2001@gmail.com>
2023-09-19 22:15:27 +0100

    Update size.c

--- size.c
+++ size.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_size - A function that calculates the size to cast the argument
+ * get_size - This function will calculate the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
